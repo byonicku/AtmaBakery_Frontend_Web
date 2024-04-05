4d1572175e8045ed7bfcc3fbe42751b269343601
jospun <joshuapuniwan03@gmail.com>
2024-04-05 22:53:55 +0700

    modalDelete dan RevisiReadProduk

--- src/page/Admin/ProdukPage.jsx
+++ src/page/Admin/ProdukPage.jsx
@@ -1,32 +1,42 @@
-import { Container, Card, Button, Col, Row, Form,Table } from "react-bootstrap"
+import { Container, Card, Button, Col, Row, Form,Table, Modal } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import "./css/ProdukPage.css";
+import { useState } from 'react';
+import { BsSearch, BsPlusSquare, BsPencilSquare, BsFillTrash3Fill, BsJournalText  } from "react-icons/bs";
 
 export default function ProdukPage() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
     return (
       <Container className="bg-style" >
-        <Container>
-          <h1>Produk</h1>
-        </Container>
         <Card>
           <Card.Header style={{ backgroundColor: "#FFFFFF" }}>
             <Row>
               <Col sm>
-                <Card.Title><h3>Produk</h3></Card.Title>
+                <Card.Title className="pt-3">
+                  <h3>Kelola Data Produk</h3>
+                  <p style={{ fontWeight: "normal", fontSize:"0.85em" }}> Lakukan pengelolaan data produk Atma Bakery</p>
+                </Card.Title>
               </Col>
-              <Col sm>
+              <Col sm className="align-self-center">
                 <Row>
-                  <Col sm="3" className="text-end">
+                  <Col sm="4" className="text-end">
                     <Button variant="success">
+                      <BsPlusSquare className="mb-1 me-2"/>
                       <Link to='./tambah' style={{ textDecoration: 'none', color: 'white' }}>
-                        Tambah Produk
+                        Tambah Data
                       </Link>
                     </Button>
                   </Col>
-                  <Col sm="8">
+                  <Col sm="7" className="m-0">
                     <Form.Control style={{ border:"1px #E5E5E5", backgroundColor:"#F2F2F2" }} type="text" placeholder="Cari Produk disini"/>
                   </Col>
-                  <Col sm="1">
-                    <Button variant="secondary"></Button>
+                  <Col sm="1" className="text-start p-0">
+                    <Button variant="secondary"><BsSearch/></Button>
                   </Col>
                 </Row>
               </Col>
@@ -36,11 +46,11 @@ export default function ProdukPage() {
             <Table className="table-striped">
               <thead>
                 <tr>
-                  <th style={{ width:"25%" }}>Nama Produk</th>
-                  <th style={{ width:"14%" }}>Kategori</th>
-                  <th style={{ width:"14%" }}>Ukuran</th>
-                  <th style={{ width:"14%" }}>Harga</th>
-                  <th style={{ width:"33%" }}></th>
+                  <th style={{ width:"25%" }} className="th-style">Nama Produk</th>
+                  <th style={{ width:"14%" }} className="th-style">Kategori</th>
+                  <th style={{ width:"14%" }} className="th-style">Ukuran</th>
+                  <th style={{ width:"14%" }} className="th-style">Harga</th>
+                  <th style={{ width:"33%" }} className="th-style"></th>
                 </tr>
               </thead>
               <tbody>
@@ -50,9 +60,9 @@ export default function ProdukPage() {
                   <td>1</td>
                   <td>350000</td>
                   <td className="text-end">
-                    <Button variant="primary" className="mx-2">Lihat Resep</Button>
-                    <Button variant="secondary" className="mx-2">Ubah Produk</Button>
-                    <Button variant="danger" className="mx-2">Hapus Produk</Button>
+                    <Button variant="primary" className="mx-2"><BsJournalText className="mb-1"/> Lihat </Button>
+                    <Button variant="secondary" className="mx-2"><BsPencilSquare className="mb-1"/> Ubah</Button>
+                    <Button variant="danger" style={{ backgroundColor:"#FF5B19" }} className="mx-2" onClick={handleShow}><BsFillTrash3Fill className="mb-1" /> Hapus</Button>
                   </td>
                 </tr>
                 <tr>
@@ -61,15 +71,38 @@ export default function ProdukPage() {
                   <td>1/2</td>
                   <td>200000</td>
                   <td className="text-end">
-                    <Button variant="primary" className="mx-2">Lihat Resep</Button>
-                    <Button variant="secondary" className="mx-2">Ubah Produk</Button>
-                    <Button variant="danger" className="mx-2">Hapus Produk</Button>
+                    <Button variant="primary" className="mx-2"><BsJournalText className="mb-1"/> Lihat </Button>
+                    <Button variant="secondary" className="mx-2"><BsPencilSquare className="mb-1"/> Ubah</Button>
+                    <Button variant="danger" style={{ backgroundColor:"#FF5B19" }} className="mx-2"  onClick={handleShow}><BsFillTrash3Fill className="mb-1" /> Hapus</Button>
                   </td>
                 </tr>
+                {/* nanti abis ini perulangan -- */}
+
               </tbody>
             </Table>
           </Card.Body>
         </Card>
+        <Modal show={show} onHide={handleClose} animation={false} centered size="lg" style={{ border:"none" }}>
+          <Modal.Body className="text-center p-5" >
+            <h3 style={{ fontWeight:"bold" }}>Anda Yakin Ingin Menghapus Data Produk Ini?</h3>
+            <p style={{ color:"rgb(18,19,20,70%)", fontSize:"1.15em" }} className="mt-3">
+              <p className="m-0 p-0">Tindakan ini tidak bisa dibatalkan.</p>
+              <p className="m-0 p-0">Semua data yang terkait dengan produk tersebut akan hilang.</p> 
+            </p>
+            <Row className="py-2 pt-3">
+              <Col sm>
+                <Button style={{ backgroundColor:"#FF5B19", border:"none" }} className="mx-2 w-100 p-1"  onClick={handleClose}>
+                  <h5 className="mt-2">Batal</h5>
+                </Button>
+              </Col>
+              <Col sm>
+                <Button style={{ backgroundColor:"#F48E28", border:"none" }} className="mx-2 w-100 p-1" >
+                  <h5 className="mt-2">Hapus</h5>
+                </Button>
+              </Col>
+            </Row>
+          </Modal.Body>
+        </Modal>
       </Container>
     )
   }
