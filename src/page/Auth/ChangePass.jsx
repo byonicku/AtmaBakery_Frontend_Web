a239210777db56d6b22bb8cd4ed29e65a518f4f8
jospun <joshuapuniwan03@gmail.com>
2024-04-09 15:40:28 +0700

    changepass verif done

--- src/page/Auth/ChangePass.jsx
+++ src/page/Auth/ChangePass.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { Button, Container, Row, Form, Image, Col } from "react-bootstrap";
 import { useMutation } from '@tanstack/react-query';
 
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams, Link } from "react-router-dom";
 import { toast } from "sonner";
 
+import notVerified from  "@/assets/images/notVerified.png";
+import verified from  "@/assets/images/verified.png";
 import InputHelper from "@/page/InputHelper";
 import APIAuth from "@/api/APIAuth";
 
@@ -21,7 +23,7 @@ export default function ResetPass() {
   const [formData, setFormData] = useState({
     password: "",
     password_confirmation: "",
-    token: key,
+    token: "", //ini nya ganti lagi ming
     email: email,
   });
 
@@ -80,54 +82,111 @@ export default function ResetPass() {
               className="p-0 m-0 rounded left-img"
             />
           </Col>
-          <Col sm style={{ backgroundColor: "#FFFFFF" }}>
-            <div className="pt-5 px-5" style={{ color: "black" }}>
-              <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>
-                <span style={{ color: "#F48E28" }}>Kata Sandi</span>
-                <span> Baru Anda</span>
-              </h1>
-              <p className="py-2" style={{ fontSize: "1em" }}>
-                Mohon masukkan kata sandi baru Anda dan harap simpan dengan baik data tersebut. 
-                Setelah berhasil masuk ke akun kembali dengan kata sandi baru!
-              </p>
-            </div>
+          {formData.token ? 
+            <Col sm style={{ backgroundColor: "#FFFFFF" }}>
+              
+              <div className="pt-5 px-5" style={{ color: "black" }}>
+                <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>
+                  <span style={{ color: "#F48E28" }}>Kata Sandi</span>
+                  <span> Baru Anda</span>
+                </h1>
+                <p className="py-2" style={{ fontSize: "1em" }}>
+                  Mohon masukkan kata sandi baru Anda dan harap simpan dengan baik data tersebut. 
+                  Setelah berhasil masuk ke akun kembali dengan kata sandi baru!
+                </p>
+              </div>
 
-            <Form className="px-5 py-2" onSubmit={inputHelper.handleSubmit}>
-              <Form.Group>
-                <Form.Label style={{ fontWeight: "bold", fontSize: "1em" }}>
-                  Kata Sandi Baru
-                </Form.Label>
-                <Form.Control
-                  style={{ border: "1px #E5E5E5", backgroundColor: "#F2F2F2" }}
-                  type="password"
-                  placeholder="Masukkan kata sandi baru"
-                  name="password"
-                  onChange={inputHelper.handleInputChange}
-                />
-              </Form.Group>
-              <Form.Group className="mt-4 mb-4">
-                <Form.Label style={{ fontWeight: "bold", fontSize: "1em" }}>
-                  Konfirmasi Kata Sandi Baru
-                </Form.Label>
-                <Form.Control
-                  style={{ border: "1px #E5E5E5", backgroundColor: "#F2F2F2" }}
-                  type="password"
-                  placeholder="Masukkan ulang kata sandi baru"
-                  name="password_confirmation"
-                  onChange={inputHelper.handleInputChange}
-                />
-              </Form.Group>
-              <Container className="text-center">
-                <Button
-                  className="button-custom"
-                  type="submit"
-                  disabled={isLoading}
-                >
-                  { isLoading ? "Loading..." : "Simpan" }
-                </Button>
-              </Container>
-            </Form>
+              <Form className="px-5 py-2" onSubmit={inputHelper.handleSubmit}>
+                <Form.Group>
+                  <Form.Label style={{ fontWeight: "bold", fontSize: "1em" }}>
+                    Kata Sandi Baru
+                  </Form.Label>
+                  <Form.Control
+                    style={{ border: "1px #E5E5E5", backgroundColor: "#F2F2F2" }}
+                    type="password"
+                    placeholder="Masukkan kata sandi baru"
+                    name="password"
+                    onChange={inputHelper.handleInputChange}
+                  />
+                </Form.Group>
+                <Form.Group className="mt-4 mb-4">
+                  <Form.Label style={{ fontWeight: "bold", fontSize: "1em" }}>
+                    Konfirmasi Kata Sandi Baru
+                  </Form.Label>
+                  <Form.Control
+                    style={{ border: "1px #E5E5E5", backgroundColor: "#F2F2F2" }}
+                    type="password"
+                    placeholder="Masukkan ulang kata sandi baru"
+                    name="password_confirmation"
+                    onChange={inputHelper.handleInputChange}
+                  />
+                </Form.Group>
+                <Container className="text-center">
+                  <Button
+                    className="button-custom"
+                    type="submit"
+                    disabled={isLoading}
+                  >
+                    { isLoading ? "Loading..." : "Simpan" }
+                  </Button>
+                </Container>
+              </Form>
+            </Col>
+          :
+          <Col sm style={{ backgroundColor: "#FFFFFF" }}>
+            <div className="pt-5 px-5 text-center" style={{ color: "black" }}>
+              <Image src={notVerified} style={{ width:"45%", height:"45%" }} className="mt-3"/>
+                <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>
+                  <span style={{ color: "#F48E28" }}>Gagal</span>
+                  <span> Untuk Verifikasi</span>
+                </h1>
+                <p className="py-2" style={{ fontSize: "1em" }}>
+                  <p className="mb-1">Tidak dapat melakukan penggantian password</p>
+                  <p className="mt-0 pt-0">Token tidak sah atau kadaluarsa</p>
+                </p>
+            </div>
+            <Container className="text-center">
+              <div className="px-5" style={{ color: "black" }}>
+                <p style={{ fontWeight: "bold", fontSize: "0.85em" }}>
+                  <span>Masuk kembali ke akun?</span>
+                  <span>
+                    {" "}
+                    <Link to="/login" style={{ textDecoration: "none" }}>
+                      Masuk ke Akun
+                    </Link>
+                  </span>
+                </p>
+              </div>
+            </Container>
+
           </Col>
+          }
+
+        {/* ini buat yang berhasil*/}
+        {/* <Col sm style={{ backgroundColor: "#FFFFFF" }}>
+            <div className="pt-5 px-5 text-center" style={{ color: "black" }}>
+              <Image src={verified} style={{ width:"45%", height:"45%" }} className="mt-3"/>
+                <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>
+                  <span style={{ color: "#F48E28" }}>Berhasil</span>
+                  <span> Ubah Kata Sandi</span>
+                </h1>
+            </div>
+            <Container className="text-center">
+              <div className="px-5" style={{ color: "black" }}>
+                <p style={{ fontWeight: "bold", fontSize: "0.85em" }}>
+                  <span>Masuk kembali dengan kata sandi terbaru!</span>
+                  <span>
+                    {" "}
+                    <Link to="/login" style={{ textDecoration: "none" }}>
+                      Masuk ke Akun
+                    </Link>
+                  </span>
+                </p>
+              </div>
+            </Container>
+
+          </Col> */}
+          
         </Row>
       </Container>
     </div>
