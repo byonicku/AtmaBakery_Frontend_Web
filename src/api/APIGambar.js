b26dbae685b4f69947eb0fcf8f4dc0bdcbf95e20
Nico Herlim <nicoherlim2003@gmail.com>
2024-04-13 11:32:01 +0700

    fix api gambar

--- src/api/APIGambar.js
+++ src/api/APIGambar.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import useAxios from "./APIConstant";
 
-const API_IMAGE = `https://api.cloudinary.com/v1_1/${
-  import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
-}/image`;
+const API_IMAGE = `https://api.cloudinary.com/v1_1/daorbrq8v/image`;
 const useAxiosImage = axios.create({ baseURL: API_IMAGE });
 
 const uploadImage = async (data, filename) => {
-  data.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
+  data.append("upload_preset", "atma-bakery");
   data.append("filename_override", filename);
   data.append("public_id", filename);
 
